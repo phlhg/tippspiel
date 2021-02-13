7223b6abb02c08cee2c5e7a4ce82f96728c99a87
Philippe Hugo <philippe.hugo@swissonline.ch>
2021-02-13 18:11:40 +0100

    [ADD] Sync state between data and ui

---
+++ client/js/h2rfp.js
@@ -0,0 +1,390 @@
+class H2RFP_Parser
+{
+    constructor()
+    {
+        this.output = [];
+
+        this.state = H2RFP_ParserState_START;
+        this.current = {name:"",id:0,data:{}};
+        this.datalen = 0;
+        this.buffer = "";
+    }
+
+    feed(streamdata) {
+        for (var i=0;i<streamdata.length;i++)
+        switch(this.state)
+        {
+        case H2RFP_ParserState_ERROR:
+            return;
+
+        case H2RFP_ParserState_START:
+            if (streamdata[i] != "!")
+            {
+                console.log("Es wird '!' am Anfang einer Anfrage erwartet.");
+                this.state = H2RFP_ParserState_ERROR;
+                return;
+            }
+            this.state = H2RFP_ParserState_NAME
+            break;
+
+        case H2RFP_ParserState_NAME:
+            if (streamdata[i] == ";")
+                this.state = H2RFP_ParserState_ID;
+            else
+                this.current.name += streamdata[i];
+            break;
+
+        case H2RFP_ParserState_ID:
+            if (streamdata[i] == ";")
+            {
+                this.current.id = Number(this.buffer);
+                if (this.current.id==NaN || this.current.id<0)
+                {
+                    console.log("Es wird eine positive ID erwartet");
+                    this.state = H2RFP_ParserState_ERROR;
+                    return;
+                }
+                this.buffer = "";
+                this.state = H2RFP_ParserState_SIZE;
+            }
+            else
+            {
+                this.buffer += streamdata[i];
+            }
+            break;
+
+        case H2RFP_ParserState_SIZE:
+            if (streamdata[i] == ";")
+            {
+                this.datalen = Number(this.buffer);
+                if (this.datalen==NaN || this.datalen<0)
+                {
+                    console.log("Es wurde eine Datenlänge erwartet");
+                    this.state = H2RFP_ParserState_ERROR;
+                    return;
+                }
+                this.buffer="";
+                this.state = H2RFP_ParserState_DATA;
+
+                if (this.datalen == 0)
+                {
+                    this.output.push(this.current);
+                    this.current = {name:"",id:0,data:{}};
+                    this.state = H2RFP_ParserState_START;
+                }
+            }
+            else
+            {
+                this.buffer += streamdata[i];
+            }
+            break;
+
+        case H2RFP_ParserState_DATA:
+            this.buffer += streamdata[i];
+            if (this.buffer.length == this.datalen)
+            {
+                try {
+                    this.current.data = JSON.parse(this.buffer);
+                } catch (e) {
+                    console.log(e);
+                    this.state = H2RFP_ParserState_ERROR;
+                    return;
+                }
+                this.buffer = "";
+                this.output.push(this.current);
+                this.current = {name:"",id:0,data:{}};
+                this.state = H2RFP_ParserState_START;
+            }
+            break;
+        }
+    }
+
+    is_good()
+    {
+        return this.state != H2RFP_ParserState_ERROR;
+    }
+
+    reset()
+    {
+        this.output = [];
+        this.state = H2RFP_ParserState_START;
+        this.current = {name:"",id:0,data:{}};
+        this.datalen = 0;
+        this.buffer = "";
+    }
+
+    static stringify(message)
+    {
+        var output = "!";
+        output += message.name;
+        output += ";";
+        output += message.id;
+        output += ";";
+        
+        var datastr = JSON.stringify(message.data);
+        output += datastr.length;
+        output += ";";
+        output += datastr;
+        return output;
+    }
+}
+
+H2RFP_ParserState_ERROR = 0;
+H2RFP_ParserState_START = 1;
+H2RFP_ParserState_NAME  = 2;
+H2RFP_ParserState_ID    = 3;
+H2RFP_ParserState_SIZE  = 4;
+H2RFP_ParserState_DATA  = 5;
+
+H2RFP_SocketState_CLOSED  = 0;
+H2RFP_SocketState_OPENING = 1;
+H2RFP_SocketState_OPEN    = 2;
+H2RFP_SocketState_CLOSING = 3;
+
+class H2RFP_Socket
+{
+    constructor(address, port)
+    {
+        if (address==undefined || port==undefined)
+            throw "address and port must be defined";
+
+        // private params
+        this.address = address;
+        this.port = port;
+        this.listeners = [];
+        this.openPromise = undefined;
+        this.closePromise = undefined;
+        this.requestPromises = [];
+        this.parser_nexti = 1;
+        this.writebuffer = "";
+
+        // private state
+        this.socket = undefined;
+        this.parser = new H2RFP_Parser();
+        
+        // private target
+        this.target = H2RFP_SocketState_CLOSED;
+
+        // public state
+        this.state = H2RFP_SocketState_CLOSED;
+
+        // public access
+        this.onConnect = ()=>{};
+        this.onDisconnect = ()=>{};
+    }
+
+    private_servemessage(msg)
+    {
+        if (msg.name.length==0)
+        {
+            for (var i=0;i<this.requestPromises.length;i++)
+            {
+                if (this.requestPromises[i][0] == msg.id)
+                {
+                    this.requestPromises[i][1].resolve(msg.data);
+                    this.requestPromises.splice(i,1);
+                    return;
+                }
+            }
+            console.log("unknown server reaction: id "+msg.id);
+            return;
+        }
+        else
+        {
+            for (var i=0;i<this.listeners.length;i++)
+            {
+                if (this.listeners[i][0] == msg.name)
+                {
+                    this.listeners[i][1].call(null,msg.data,msg.id);
+                    return;
+                }
+            }
+            console.log("unknwon server event: "+msg.name);
+            return;
+        }
+    }
+
+    private_react()
+    {
+        if (this.target == H2RFP_SocketState_OPEN)
+        {
+            if (this.state==H2RFP_SocketState_CLOSED && this.parser.state == H2RFP_ParserState_ERROR)
+            {
+                this.parser.reset();
+                // continue with react()!
+            }
+            if (this.state==H2RFP_SocketState_OPENING && this.parser.state == H2RFP_ParserState_ERROR)
+            {
+                throw "Dieser Status sollte nicht existieren!";
+            }
+            if (this.state==H2RFP_SocketState_OPEN && this.parser.state == H2RFP_ParserState_ERROR)
+            {
+                this.state = H2RFP_SocketState_CLOSING;
+                this.socket.close();
+                this.parser.reset();
+            }
+            if (this.state==H2RFP_SocketState_CLOSING && this.parser.state == H2RFP_ParserState_ERROR)
+            {
+                this.parser.reset();
+            }
+            if (this.state==H2RFP_SocketState_CLOSED && this.parser.state != H2RFP_ParserState_ERROR)
+            {
+                this.socket = new WebSocket("ws://" + this.address + ":" + this.port);
+                this.socket.onopen = this.private_onopen.bind(this);
+                this.socket.onmessage = this.private_onmessage.bind(this);
+                this.socket.onclose = this.private_onclose.bind(this);
+                this.socket.onerror = this.private_onerror.bind(this);
+                this.state = H2RFP_SocketState_OPENING;
+            }
+            if (this.state==H2RFP_SocketState_OPENING && this.parser.state != H2RFP_ParserState_ERROR)
+            {
+                // just waiting
+            }
+            if (this.state==H2RFP_SocketState_OPEN && this.parser.state != H2RFP_ParserState_ERROR)
+            {
+                if (this.openPromise!=undefined)
+                    this.openPromise.resolve();
+                this.openPromise = undefined;
+                if (this.writebuffer.length>0)
+                    this.socket.send(this.writebuffer);
+                this.writebuffer = "";
+            }
+            if (this.state==H2RFP_SocketState_CLOSING && this.parser.state != H2RFP_ParserState_ERROR)
+            {
+                // just waiting
+            }
+        }
+        else if (this.target == H2RFP_SocketState_CLOSED)
+        {
+            if (this.state==H2RFP_SocketState_CLOSED && this.parser.state == H2RFP_ParserState_ERROR)
+            {
+                this.parser.reset();
+            }
+            if (this.state==H2RFP_SocketState_OPENING && this.parser.state == H2RFP_ParserState_ERROR)
+            {
+                throw "Dieser Status sollte nicht existieren!"
+            }
+            if (this.state==H2RFP_SocketState_OPEN && this.parser.state == H2RFP_ParserState_ERROR)
+            {
+                this.parser.reset();
+                this.state = H2RFP_SocketState_CLOSING;
+                this.socket.close();
+            }
+            if (this.state==H2RFP_SocketState_CLOSING && this.parser.state == H2RFP_ParserState_ERROR)
+            {
+                this.parser.reset();
+            }
+            if (this.state==H2RFP_SocketState_CLOSED && this.parser.state != H2RFP_ParserState_ERROR)
+            {
+                if (this.openPromise!=undefined)
+                    this.openPromise.reject("connection failed");
+                this.openPromise = undefined;
+                if (this.closePromise!=undefined)
+                    this.closePromise.resolve();
+                this.closePromise = undefined;
+                this.requestPromises.forEach((e,i,a)=>{e[1].reject("connection failed")});
+                this.requestPromises = [];
+            }
+            if (this.state==H2RFP_SocketState_OPENING && this.parser.state != H2RFP_ParserState_ERROR)
+            {
+                // just waiting
+            }
+            if (this.state==H2RFP_SocketState_OPEN && this.parser.state != H2RFP_ParserState_ERROR)
+            {
+                this.state = H2RFP_SocketState_CLOSING;
+                this.socket.close();
+            }
+            if (this.state==H2RFP_SocketState_CLOSING && this.parser.state != H2RFP_ParserState_ERROR)
+            {
+                // just waiting
+            }
+        }
+        else
+        {
+            throw "Ungültiger Zielstatus";
+        }
+    }
+
+    private_onopen()
+    {
+        this.state = H2RFP_SocketState_OPEN;
+        this.private_react();
+        this.onConnect.call();
+    }
+
+    private_onmessage(message)
+    {
+        this.parser.feed(message.data);
+        if (this.parser.state != H2RFP_ParserState_ERROR)
+        {
+            this.parser.output.forEach(((e,i,a)=>{
+                this.private_servemessage(e);
+            }).bind(this));
+            this.parser.output = [];
+        }
+        this.private_react();
+    }
+
+    private_onclose()
+    {
+        this.target = H2RFP_SocketState_CLOSED; // do not retry
+        this.state = H2RFP_SocketState_CLOSED;
+        this.private_react();
+        this.onDisconnect.call();
+    }
+
+    private_onerror()
+    {
+        // parallel to onclose
+    }
+
+    open()
+    {
+        this.target = H2RFP_SocketState_OPEN;
+        var out = new Promise(((res,rej)=>{
+            this.openPromise={resolve:res,reject:rej}
+        }).bind(this));
+        this.private_react();
+        return out;
+    }
+
+    close()
+    {
+        this.target = H2RFP_SocketState_CLOSED;
+        var out = new Promise(((res,rej)=>{
+            this.closePromise={resolve:res,reject:rej}
+        }).bind(this));
+        this.private_react();
+        return out;
+    }
+
+    listen(evName,callback)
+    {
+        this.listeners.push([evName,callback]);
+    }
+
+    notify(evName, evData)
+    {
+        if (evData==undefined)
+            throw "evData undefined";
+        var serial = H2RFP_Parser.stringify({name:evName,id:0,data:evData});
+        if (this.state == H2RFP_SocketState_OPEN)
+            this.socket.send(serial);
+        else
+            this.writebuffer += serial;
+    }
+
+    exec(evName, evData)
+    {
+        var serial = H2RFP_Parser.stringify({name:evName,id:this.parser_nexti,data:evData});
+        if (this.state == H2RFP_SocketState_OPEN)
+            this.socket.send(serial);
+        else
+            this.writebuffer += serial;
+        var out = new Promise(((res,rej)=>{
+            this.requestPromises.push([this.parser_nexti,{resolve:res,reject:rej}]);
+        }).bind(this));
+        this.parser_nexti = ((++this.parser_nexti)%9999)+1;
+        return out;
+    }
+}
--- client/js/main.js
+++ client/js/main.js
@@ -8,34 +8,47 @@ window.addEventListener("DOMContentLoaded",function(){
 class App {
 
     constructor(){
-        this.router = new Router();
+        this.router = new Router(this);
+        this.models = {}
+        this.models.games = new GameList();
 
         this.setRoutes()
-        this.setEvents()
+        this.setGlobalEvents()
 
         this.router.find(window.location.pathname);
     }
 
     setRoutes(){
-        this.router.add("/",HomeController);
         this.router.setErrorHandler(ErrorController);
+        this.router.add("/",HomeController);
+        this.router.add("/profile/",ProfileController);
+        this.router.add("/signin/",SignInController);
+        this.router.add("/signup/",SignUpController);
     }
 
     /** Adds global event listeners */
-    setEvents(){
+    setGlobalEvents(){
         setTimeout(function(){ document.body.classList.remove("loading"); },1000);
 
-        Array.from(document.querySelectorAll("a[href]")).filter(a => {
+        this.setEvents(document.body);
+
+        window.addEventListener("popstate", e => {
+            this.router.find(window.location.pathname);
+        })
+
+        document.addEventListener("DOMNodeRemoved", e => {
+            e.target.dispatchEvent(new Event("removed"));
+        });
+    }
+
+    setEvents(root){
+        Array.from(root.querySelectorAll("a[href]")).filter(a => {
             return a.getAttribute("href").indexOf("http") != 0 && a.getAttribute("href").indexOf("//") != 0
         }).forEach(a => {
             a.onclick = e => {
                 e.preventDefault();
-                this.router.run(a.getAttribute("href"));
+                this.router.load(a.getAttribute("href"));
             }
-        })
-
-        window.addEventListener("popstate", e => {
-            this.router.find(window.location.pathname);
         })
     }
 
@@ -44,7 +57,8 @@ class App {
 
 class Router {
 
-    constructor(){
+    constructor(app){
+        this.app = app
         this.root = document.querySelector(".tipp-section-wrapper");
         this.error = null;
         this.routes = [];
@@ -59,20 +73,20 @@ class Router {
     }
 
     find(path){
-        this.routes.forEach(r => r.root.classList.remove("active"))
-        this.error.root.classList.remove("active");
+        this.routes.forEach(r => r.unload())
+        this.error.unload()
         setTimeout(() => {
             let route = this.routes.find(r => r.matches(path));
             console.log("Navigating to "+path);
             if(route == undefined){
-                this.error.run({}); 
+                this.error.load(); 
             } else {
                 route.take(path);
             }
         },400);
     }
 
-    run(path){
+    load(path){
         window.history.pushState({}, '', path);
         return this.find(path);
     }
@@ -106,13 +120,17 @@ class Route {
     take(path){
         if(!this.matches(path)){ return false; }
         var match = this.pattern.exec(path);
-        return this.run(match.groups ? match.groups : {});
+        return this.load(match.groups ? match.groups : {});
     }
 
-    run(params){
-        this.root.classList.add("active")
-        this.controller.run(params);
+    load(params){
+        params = params ?? {}
+        this.controller._load(params);
         return true;
+    }
+
+    unload(){
+        this.controller._unload();
     }
 
 }
@@ -120,110 +138,299 @@ class Route {
 class Controller {
     
     constructor(route){
+        this.active = false;
         this.route = route;
+        this.router = this.route.router;
+        this.app = this.router.app;
+
+        this.params = {};
+        this.models = this.app.models;
         this.dom = {}
         this.dom.root = this.route.root;
+
+        this.init();
     }
 
-    run(params){
-        console.log(params);
+    init(){
+        console.log("Initialized")
+    }
+
+    _load(params){
+        this.active = true;
+        this.params = params ?? {};
+        this.load();
+        this.dom.root.classList.add("active");
+        this.app.setEvents(this.dom.root);
+    }
+
+    load(){
+        console.log("Loaded with ",this.params)
+    }
+
+    _unload(){
+        this.dom.root.classList.remove("active")
+        if(this.active){ setTimeout(() => { this.unload() }, 400); }
+    }
+
+    unload(){
+        console.log("Unloaded");
     }
 
 }
 
 class ErrorController extends Controller {
 
-    constructor(root){
-        super(root);
+    constructor(...args){
+        super(...args);
         this.dom.root.classList.add("tipp-error-page");
         this.dom.root.innerHTML = `<div class="inner"><h3>Whoops...</h3><p>Sorry for the inconvenience, but this seems like a deadend. We will investigate this further.</p><a href="/" class="button">Return to Home</a></div>`;
         this.dom.root.querySelector(".button").onclick = e => {
             e.preventDefault();
-            this.router.run("/");
+            this.router.load("/");
         }
     }
 
-    run(params){
-        
+}
+
+class ProfileController extends Controller {
+
+    constructor(...args){
+        super(...args);
+    }
+
+    load(){
+        this.router.load("/signup/");
+    }
+
+}
+
+class SignUpController extends Controller {
+
+    constructor(...args){
+        super(...args);
+    }
+
+    init(){
+        this.dom.root.classList.add("tipp-login-page");
+        this.dom.root.innerHTML = `<div class="inner">
+            <h3>Registrieren</h3>
+            <p>Gib deinen Namen und deine E-Mail ein und erhalte von uns einen Zugangscode.</p>
+            <form>
+                <input type="name" placeholder="Name: z.B. Max Mustermann" />
+                <input type="email" placeholder="E-Mail: z.B. max.mustermann@beispiel.ch" />
+                <input type="submit" value="Registrieren"/>
+            </form>
+            <span class="meta">Falls du schon einen Account hast, <a href="/signin/">melde dich an</a></span>
+        </div>`;
+    }
+
+}
+
+class SignInController extends Controller {
+
+    constructor(...args){
+        super(...args);
+    }
+
+    init(){
+        this.dom.root.classList.add("tipp-login-page");
+        this.dom.root.innerHTML = `<div class="inner">
+            <h3>Anmelden</h3>
+            <p>Gib deinen Zugangscode <i>aus der E-Mail</i>, die wir dir gesendet haben, ein.</p>
+            <form>
+                <input type="text" pattern="TPS[0-9]{9}" placeholder="Zugangscode: z.B. TPS123456789" />
+                <input type="submit" value="Anmelden"/>
+            </form>
+            <span class="meta">Falls du noch keinen Account hast, <a href="/signup/">registriere dich</a></span>
+        </div>`
     }
 
 }
 
 class HomeController extends Controller {
 
-    constructor(root){
-        super(root);
-
-        const game1 = new Game();
-        const game2 = new Game();
-        const game3 = new Game();
-
-        this.dom.root.appendChild(game1.getHtml());
-
-        game1.data.t1Name = "SUI";
-        game1.data.t2Name = "ESP";
-        game1._updateDom();
-
-        game1.dom.wrapper.classList.add("live")
-
-        this.dom.root.appendChild(game2.getHtml());
-        game2.data.t1Name = "GER";
-        game2.data.t2Name = "FRA";
-        game2._updateDom();
-
-        this.dom.root.appendChild(game3.getHtml());
-        game3.data.t1Name = "RUS";
-        game3.data.t2Name = "UKR";
-        game3._updateDom();
-
+    constructor(...args){
+        super(...args);
     }
 
-    run(params){
+    load(){
+        this.models.games.get([1,2]).forEach(game => {
+            let view = new GameTileView(game)
+            this.dom.root.appendChild(view.getHtml());
+        });
+    }
 
+    unload(){
+        this.dom.root.innerHTML = "";
     }
 
 }
 
-console.log(HomeController);
-console.log(Controller);
+class GameTileView {
+    
+    constructor(data){
+        this.data = data;
+        this.dom = {};
+        this._setupDom();
+        this._updateDom();
+        this._func = function(e){
+            if(e.detail?.type == "game" && e.detail?.id == this.data.id)
+                this._updateDom();
+        }.bind(this);
+        window.addEventListener("datachange", this._func);
+        this.dom.wrapper.addEventListener("removed",e => {
+            window.removeEventListener("datachange",this._func)
+        });
+    }
 
-function Game(){
-    this.dom = {};
-    this.data = {};
-    this.data.t1Name = "";
-    this.data.t2Name = "";
-    this.data.t1Points = 0;
-    this.data.t2Points = 0;
-    this.data.kickoff = new Date();
-    this.data.stream = "";
-    this.data.tipps = 0;
-    this._setupDom();
-    this._updateDom();
+    _setupDom(){
+        this.dom.wrapper = document.createElement("a");
+        this.dom.wrapper.setAttribute("href",`/game/${this.data.id}/${this.data.team1.name.toLowerCase()}-${this.data.team2.name.toLowerCase()}/`)
+        this.dom.wrapper.classList.add("game-tile");
+        this.dom.wrapper.innerHTML = `<span class="live-indicator">live</span><span class="title"><span class="tflag"></span> <span class="t"></span> <span class="tflag"></span></span><span class="meta"></span><div class="live-view"><iframe allowfullscreen></iframe></div>`;
+        this.dom.title = this.dom.wrapper.querySelector(".title > .t")
+        this.dom.flag1 = this.dom.wrapper.querySelectorAll(".title .tflag")[0]
+        this.dom.flag2 = this.dom.wrapper.querySelectorAll(".title .tflag")[1]
+        this.dom.meta = this.dom.wrapper.querySelector(".meta");
+        this.dom.iframe = this.dom.wrapper.querySelector("iframe");
+    }
+
+    _updateDom(){
+        this.dom.title.innerText = `${this.data.team1.name} ${this.data.team1.points}:${this.data.team2.points} ${this.data.team2.name}`;
+        this.dom.flag1.setAttribute("data-t",this.data.team1.name.toLowerCase());
+        this.dom.flag2.setAttribute("data-t",this.data.team2.name.toLowerCase());
+        this.dom.meta.innerText = `${("0"+this.data.start.getDate()).slice(-2)}.${("0"+(this.data.start.getMonth()+1)).slice(-2)} ${("0"+this.data.start.getHours()).slice(-2)}:${("0"+this.data.start.getMinutes()).slice(-2)} | ${this.data.tippsCount} Tipps`;
+    }
+
+    getHtml(){
+        return this.dom.wrapper;
+    }
+
 }
 
-Game.prototype._setupDom = function(){
-    this.dom.wrapper = document.createElement("a");
-    this.dom.wrapper.classList.add("game-tile");
-    this.dom.wrapper.innerHTML = `<span class="live-indicator">live</span><span class="title"><span class="tflag"></span> <span class="t"></span> <span class="tflag"></span></span><span class="meta"></span><div class="live-view"><iframe allowfullscreen></iframe></div>`;
-    this.dom.title = this.dom.wrapper.querySelector(".title > .t")
-    this.dom.flag1 = this.dom.wrapper.querySelectorAll(".title .tflag")[0]
-    this.dom.flag2 = this.dom.wrapper.querySelectorAll(".title .tflag")[1]
-    this.dom.meta = this.dom.wrapper.querySelector(".meta");
-    this.dom.iframe = this.dom.wrapper.querySelector("iframe");
+class GameList {
+
+    constructor(){
+        this.list = {};
+        this.list[1] = new Game({
+            id: 1,
+            start: 0,
+            status: Game.UPCOMING,
+            progress: Game.NORMAL,
+            location: "Bern, Stade de Suisse",
+            tippsCount: 2,
+            team1: {
+                name: "SUI",
+                points: 0,
+                pointsExt: 0,
+                pointsPenalty: 0,
+                scorers: []
+            },
+            team2: {
+                name: "ESP",
+                points: 0,
+                pointsExt: 0,
+                pointsPenalty: 0,
+                scorers: []
+            }
+        });
+        this.list[2] = new Game({
+            id: 2,
+            start: 0,
+            status: Game.UPCOMING,
+            progress: Game.NORMAL,
+            location: "Zürich, Letzigrund",
+            tippsCount: 0,
+            team1: {
+                name: "GER",
+                points: 0,
+                pointsExt: 0,
+                pointsPenalty: 0,
+                scorers: []
+            },
+            team2: {
+                name: "FRA",
+                points: 0,
+                pointsExt: 0,
+                pointsPenalty: 0,
+                scorers: []
+            }
+        });
+    }
+
+    get(ids){
+        return ids.map(id => {
+            if(this.list.hasOwnProperty(id))
+                return this.list[id];
+        })
+    }
+
 }
 
-Game.prototype._updateDom = function(){
-    this.dom.title.innerText = `${this.data.t1Name} ${this.data.t1Points}:${this.data.t2Points} ${this.data.t2Name}`;
-    this.dom.flag1.setAttribute("data-t",this.data.t1Name.toLowerCase());
-    this.dom.flag2.setAttribute("data-t",this.data.t2Name.toLowerCase());
-    this.dom.meta.innerText = `${("0"+this.data.kickoff.getDate()).slice(-2)}.${("0"+(this.data.kickoff.getMonth()+1)).slice(-2)} ${("0"+this.data.kickoff.getHours()).slice(-2)}:${("0"+this.data.kickoff.getMinutes()).slice(-2)} | ${this.data.tipps} Tipps`;
+class Game {
+
+    constructor(data){
+        this.id = -1;
+        this.start = new Date(0);
+        this.status = Game.UPCOMING;
+        this.progress = Game.NORMAL;
+        this.location = "";
+        this.tippsCount = 0;
+
+        this.team1 = {}
+        this.team1.name = "TM1";
+        this.team1.points = 0;
+        this.team1.pointsExt = 0;
+        this.team1.pointsPenalty = 0;
+        this.team1.scorers = [];
+
+        this.team2 = {}
+        this.team2.name = "TM2";
+        this.team2.points = 0;
+        this.team2.pointsExt = 0;
+        this.team2.pointsPenalty = 0;
+        this.team2.scorers = [];
+
+        this._update(data);
+    }
+
+    _update(data){
+        this.id = data.id ?? this.id;
+        this.start = data.start ? new Date(data.start) : this.start;
+        this.status = data.status ?? this.status;
+        this.progress = data.progress ?? this.progress;
+        this.location = data.location ?? this.location;
+        this.tippsCount = data.tippsCount ?? this.tippsCount;
+
+        this.team1.name = data.team1?.name ?? this.team1.name;
+        this.team1.points = data.team1?.points ?? this.team1.points;
+        this.team1.pointsExt = data.team1?.pointsExt ?? this.team1.pointsExt;
+        this.team1.pointsPenalty = data.team1?.pointsPenalty ?? this.team1.pointsPenalty;
+        this.team1.scorers = data.team1?.scorers ?? this.team1.scorers;
+
+        this.team2.name = data.team2?.name ?? this.team2.name;
+        this.team2.points = data.team2?.points ?? this.team2.points;
+        this.team2.pointsExt = data.team2?.pointsExt ?? this.team2.pointsExt;
+        this.team2.pointsPenalty = data.team2?.pointsPenalty ?? this.team2.pointsPenalty;
+        this.team2.scorers = data.team2?.scorers ?? this.team2.scorers;
+    }
+
+    update(data){
+        this._update(data);
+        window.dispatchEvent(new CustomEvent("datachange",{
+            detail: { type: "game", id: this.id }
+        }))
+    }
+
 }
 
-Game.prototype.getHtml = function(){
-    return this.dom.wrapper;
-}
+/** Enumerations for Game.state */
+Game.UPCOMING = 0;
+Game.RUNNING = 1;
+Game.PENDING = 2;
+Game.ENDED = 3;
 
-
-
-Game.prototype = Object.create(Game.prototype)
-Game.prototype.constructor = Game
+/** Enumerations for Game.progress */
+Game.NORMAL = 0;
+Game.OVERTIME = 1;
+Game.PENALTY = 2;
