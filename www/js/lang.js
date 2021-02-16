4156dca4a78fe83f6996b25e065f3d233c961c0a
LeTobi <tohub@bluewin.ch>
2021-02-16 14:58:40 +0100

    [update] error enum

--- www/js/lang.js
+++ www/js/lang.js
@@ -1,7 +1,11 @@
 LANG = {
     de: {
         errors: {
-            0: "Es ist ein unbekannter Fehler aufgetreten"
+            0: "Alles ist in Ordnung",
+            1: "Der Server verlangt eine Authentifizierung",
+            2: "{email} wird bereits verwendet",
+            3: "{name} nimmt bereits am Tippspiel teil",
+            4: "Du konntest nicht identifiziert werden."
         }
     }
 }
