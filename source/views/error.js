27e189657a140631e1943e3a171ed4da2ae8db7f
Philippe Hugo <philippe.hugo@swissonline.ch>
2021-02-16 22:30:17 +0100

    [update] translate error page

--- source/views/error.js
+++ source/views/error.js
@@ -8,7 +8,7 @@ export default class Error extends View {
 
     init(){
         this.root.classList.add("tipp-error-page");
-        this.root.innerHTML = `<div class="inner"><h3>Whoops...</h3><p>Sorry for the inconvenience, but this seems like a deadend. We will investigate this further.</p><a href="/" class="button">Return to Home</a></div>`;
+        this.root.innerHTML = `<div class="inner"><h3>Hoppla...</h3><p>Das sieht nach einer Sackgasse aus.<br/>Wir werden dies untersuchen.</p><a href="/" class="button">Zurück zu Start</a></div>`;
         this.button = this.root.querySelector(".button");
     }
 
