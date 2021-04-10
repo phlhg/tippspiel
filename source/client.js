47c44cfa0f48335d455c659af3e28aeb68461566
Philippe Hugo <philippe.hugo@swissonline.ch>
2021-04-10 20:50:38 +0200

    Small fixes

--- source/client.js
+++ source/client.js
@@ -125,7 +125,7 @@ export default class Client {
     signout(){
         localStorage.setItem("tipp-dev-iskown","true")
         localStorage.removeItem("tipp-dev-token")
-        window.location.reload()
+        window.location = "/"
     }
 
 
