fea278e8f2cfe02904506527399989ee725d9726
Philippe Hugo <philippe.hugo@swissonline.ch>
2021-05-04 19:22:21 +0200

    Added form loading animation, See #1

--- source/views/helpers/form.js
+++ source/views/helpers/form.js
@@ -3,6 +3,7 @@ export default class Form {
     constructor(root){
 
         this.root = root;
+
         this.dom = {}
         this.dom.submit = this.root.querySelector("input[type=submit]");
 
@@ -11,21 +12,38 @@ export default class Form {
         this.dom.error = document.createElement("span");
         this.dom.error.classList.add("error");
 
+        this.dom.loader = document.createElement("span");
+        this.dom.loader.classList.add("loader");
+
         this.root.insertBefore(this.dom.info, this.dom.submit);
         this.root.insertBefore(this.dom.error, this.dom.submit);
+        this.root.appendChild(this.dom.loader);
 
-        this.onSubmit = (data) => {}
+        this.onSubmit = async (data) => {}
+
+        this.isSubmitting = false;
 
         this.setEvents();
+
     }
 
     setEvents(){
-        this.root.addEventListener("submit",e => {
+        this.root.addEventListener("submit",async e => {
             e.preventDefault();
+            
+            // Check if form is already submitting
+            if(this.isSubmitting){ return; }
+            this.isSubmitting = true;
+
             this.info(""); 
             this.error("");
             let data = Object.fromEntries(new FormData(e.target).entries());
-            this.onSubmit(data)
+            this.dom.loader.classList.add("active");
+
+            await this.onSubmit(data);
+
+            this.dom.loader.classList.remove("active");
+            this.isSubmitting = false;
         })
     }
 
