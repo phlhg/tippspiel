2e268dfefd1f7c4f36581ccfc6cb41e8758ee98b
Philippe Hugo <philippe.hugo@swissonline.ch>
2021-05-09 22:41:52 +0200

    Removed alpha notice

--- source/views/home.js
+++ source/views/home.js
@@ -10,16 +10,7 @@ export default class Home extends View {
 
     init(){
         this.root.innerHTML = 
-        `<div class="tipp-box" style="background:rgb(240,100,0);">
-            <span class="icon"><span class="material-icons">science</span></span>
-            <span class="title">ALPHA Version</span>
-            <span class="meta">
-                Bitte probiere die folgenden Funktionen aus und <u>informiere uns über Fehler und mögliche Verbesserungen</u>: Registrierung, An- & Abmelden, Tippen, Profil und Sprachen<br/>
-                <br/>
-                <b>Tipp</b> Installiere diese Seite als App mit <i>"Zum Homescreen hinzufügen"</i> unter <span class="material-icons">ios_share</span> oder <i>"Installieren"</i> unter <span class="material-icons">more_vert</span>
-            </span>
-        </div>
-        <div class="tipp_home_event" style="margin-bottom: 20px;"></div>
+        `<div class="tipp_home_event" style="margin-bottom: 20px;"></div>
         <div class="tipp_home_upcoming"></div>
         <h3>${Lang.get("section/home/pastgames")}</h3>
         <div class="tipp_home_over"></div>`
