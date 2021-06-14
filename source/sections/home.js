674af399d02d5f870142d8b4f8e626067f15d8b0
Philippe Hugo <philippe.hugo@swissonline.ch>
2021-06-14 13:51:50 +0200

    Removed event 2 from home

--- source/sections/home.js
+++ source/sections/home.js
@@ -42,9 +42,7 @@ export default class Home extends Section {
         
         // Events
         this.view.eventList.innerHTML = '';
-        App.model.events.getAll([1,2]).forEach(e => {
-            this.view.eventList.appendChild(new EventTile(e).getHtml())
-        })
+        this.view.eventList.appendChild(new EventTile(App.model.events.get(1)).getHtml())
 
         this.view.eventTipp.style.display = "none";
         App.model.events.get(1).then(event => {
